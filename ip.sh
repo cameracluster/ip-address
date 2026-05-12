911f43ec6eae659e82fbf0725a9537a95f8c2619
cameracluster <142507537+cameracluster@users.noreply.github.com>
2026-05-12 15:05:30 -0400

    updates shell script

--- ip.sh
+++ ip.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-
 curl -s -i -H "Accept: application/json" 'http://api.ipapi.com/api/check?access_key=...&fields=main' | tail -n 1 | jq -M -r '.country_name,.city,.continent_name,.ip'
